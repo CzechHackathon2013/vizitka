61c030831c3e32a9cf76e3124b6b2eee6e548b45
David Barton <david@dreamorreal.com>
2013-10-19 23:39:01 +0200

    Markdown parser

--- clientapp/libraries/rune/rune.js
+++ clientapp/libraries/rune/rune.js
@@ -1,4 +1,17 @@
 var jade = require("jade");
+var marked = require("marked");
+var async = require("async");
+
+marked.setOptions({
+  gfm: true,
+  highlight: null,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
 
 /**
  *
@@ -6,6 +19,8 @@ var jade = require("jade");
  * @param callback (err, result html)
  */
 exports.renderPage = function(content, callback) {
+  content = unmarkPage(content);
+  console.error(content);
   var filename = __dirname + '/../../../clienttemplates/themes/' + content.theme + '/index.jade';
   var opt = { filename: content.theme + "/index", pretty: true, data: content };
   jade.renderFile(filename, opt, function(err, html) {
@@ -20,6 +35,7 @@ exports.renderPage = function(content, callback) {
  * @param callback (err, result html)
  */
 exports.renderBrick = function(brickData, theme, callback) {
+  brickData = unmarkBrick(brickData);
   var filename = __dirname + '/../../../clienttemplates/themes/' + theme + '/' + brickData.type + '.jade';
   var opt = { filename: theme + '/' + brickData['type'], pretty: true, context: brickData.content };
   jade.renderFile(filename, opt, function(err, html) {
@@ -27,3 +43,16 @@ exports.renderBrick = function(brickData, theme, callback) {
   });
 };
 
+var unmarkPage = function(content){
+  for (var i = 0; i < content.length; i++) {
+    content.bricks[i] = unmarkBrick(content.bricks[i]);
+  }
+  return content;
+};
+
+var unmarkBrick = function(brickData){
+  for (var key in brickData.content) {
+    brickData.content[key] = marked(brickData.content[key]);
+  }
+  return brickData;
+};
